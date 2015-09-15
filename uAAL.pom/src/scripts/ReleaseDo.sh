63fb5f145d420509101143815f46e035e8da12e7
Alejandro Medrano Gil <amedrano@lst.tfo.upm.es>
2015-09-15 20:01:06 +0200

    some fixes to ReleaseDo script.

--- uAAL.pom/src/scripts/ReleaseDo.sh
+++ uAAL.pom/src/scripts/ReleaseDo.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+MY_DIR=`dirname $0`
+
+. $MY_DIR/ReleaseCommon.sh
 
 VERSION=$1
 #checkout prereleases
@@ -11,11 +14,13 @@ checkAndDeploy
 #Tag
 gitAll tag $VERSION
 
+#update local
+gitAll checkout master
+
 #delete prereleases
 gitAll branch -d prerelease_$VERSION
 
 #push tags and delete remote prereleases
 gitAll push --tags origin :prerelease_$VERSION
 
-#update local
-gitAll checkout master
+
