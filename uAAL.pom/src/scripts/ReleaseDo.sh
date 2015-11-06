324f3091cb64d1a4ac4822480b7884c8b4182817
Alejandro Medrano Gil <amedrano@lst.tfo.upm.es>
2015-11-06 21:19:33 +0100

    added manual merge procedure reminder in release script.

--- uAAL.pom/src/scripts/ReleaseDo.sh
+++ uAAL.pom/src/scripts/ReleaseDo.sh
@@ -14,9 +14,19 @@ gitAll tag $VERSION
 #update local
 gitAll checkout master
 
+echo "manually do:" 
+echo "git merge prerelease_$VERSION"
+echo "mvn deploy -Prelease"
+echo 
+echo
+read -rsp $'Press any key to continue...\n' -n1 key
+
+
 #delete prereleases
 gitAll branch -d prerelease_$VERSION
 
 #push tags and delete remote prereleases
 gitAll push --tags origin :prerelease_$VERSION
+
+
 
