ec5e03f09b710f91208f4693395613dc646bbe53
cstockloew <stockloew@gmx.de>
2017-05-18 20:02:35 +0200

    added script for uploading site to gh-pages branch for platform repo only; maybe this could be integrated into ci.sh?

---
+++ uAAL.pom/src/scripts/push-to-gh-pages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "cstockloew/platform" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  echo -e "Publishing...\n"
+  set -x
+
+  cp -R "target/site" $HOME/site
+    
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/cstockloew/platform gh-pages > /dev/null
+
+  cd gh-pages
+  git rm --ignore-unmatch -rf . > /dev/null
+  cp -Rf $HOME/site/* .
+  git add -f . > /dev/null
+  git commit -m "Latest site on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"  > /dev/null
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Published site to gh-pages.\n"
+  
+fi
